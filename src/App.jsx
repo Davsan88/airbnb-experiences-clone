130b53e950fa921be2dfa7d6e317a57b204f9788
DavSan <david.sanchez.martinez@outlook.com>
2024-09-23 17:41:34 +0100

    Imported Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 import './App.css'
+
 
 function App() {
   return (
     <>
      <Navbar />
+     <Hero />
     </>
   )
 }
