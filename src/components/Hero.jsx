842b15ffa0db274011db3dc852907193a9e6fef9
DavSan <david.sanchez.martinez@outlook.com>
2024-09-23 17:39:21 +0100

    Create Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../App.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <img src="airbnb-experiences-grid.jpg" alt="Grid of different images" />
+      <div>
+        <h1>Online Experiencies</h1>
+        <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
