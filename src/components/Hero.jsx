b7eb412aa43593fafd7443b44f6e7f21f5289b4a
DavSan <david.sanchez.martinez@outlook.com>
2024-09-23 18:50:17 +0100

    Added more semantic naming

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
   return (
     <div className='hero'>
       <img  className="hero__img" src="airbnb-experiences-grid.jpg" alt="Grid of different images" />
-      <div className="hero__div" >
-        <h1 className="hero__div__h1" >Online Experiencies</h1>
-        <p className="hero__div__p" >Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      <div className="hero__content" >
+        <h1>Online Experiences</h1>
+        <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
       </div>
     </div>
   )
