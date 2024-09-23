1cc15ae03e654adb2dda3ce8340b6a2f624f7580
DavSan <david.sanchez.martinez@outlook.com>
2024-09-23 16:44:10 +0100

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="/airbnb-logo.png" alt="Airbnb logo" />
+    </nav>
+  )
+}
+
+export default Navbar
