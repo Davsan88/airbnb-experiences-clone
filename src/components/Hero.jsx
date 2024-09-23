76a4afcc9c60c5530cda7e53a30b1d2ababc8037
DavSan <david.sanchez.martinez@outlook.com>
2024-09-23 17:43:54 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,10 @@ import '../App.css'
 const Hero = () => {
   return (
     <div className='hero'>
-      <img src="airbnb-experiences-grid.jpg" alt="Grid of different images" />
-      <div>
-        <h1>Online Experiencies</h1>
-        <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      <img  className="hero__img" src="airbnb-experiences-grid.jpg" alt="Grid of different images" />
+      <div className="hero__div" >
+        <h1 className="hero__div__h1" >Online Experiencies</h1>
+        <p className="hero__div__p" >Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
       </div>
     </div>
   )
