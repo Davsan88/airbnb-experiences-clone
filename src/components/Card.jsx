10a24438a07b31fceb66aa0a48c7c2344e321731
DavSan <david.sanchez.martinez@outlook.com>
2024-09-24 09:30:56 +0100

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,12 @@ import '../App.css'
 
 const Card = () => {
   return (
-    <div>
-        <div>
+    <div className='card'>
+        <div className='card__image'>
             <img src="katie-zaferes.png" alt="American professional triathlete Katie Zaferes" />
             <p>SOLD OUT</p>
         </div>
-        <div>
+        <div className='card__info'>
             <i class="fa-solid fa-star"></i>
             <p>
                 (6)·USA <br />
