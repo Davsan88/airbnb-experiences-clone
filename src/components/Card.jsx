f3fc7970df32b303a89771895ed1445ed3c60859
DavSan <david.sanchez.martinez@outlook.com>
2024-09-26 12:48:29 +0100

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import '../App.css'
 
-const Card = ({img, status, rating, reviewCount, country, title, price}) => {
+const Card = ({ img, status, rating, reviewCount, country, title, price }) => {
+  console.log({ img, status, rating, reviewCount, country, title, price }); // Check props
+
+
   return (
+
     <div className='card'>
-        <img className='card__image' src={img} alt="American professional triathlete Katie Zaferes" />
-        <span className='card__status'>{status}</span>
+      <img className='card__image' src={img} alt="American professional triathlete Katie Zaferes" />
+      <span className='card__status'>{status}</span>
       <div className='card__info'>
-        <img src="star.png" alt="Star icon" className='card__star'/>
+        <img src="star.png" alt="Star icon" className='card__star' />
         <span>{rating} </span>
         <span className='gray'>{reviewCount} ·</span>
         <span className='gray'>{country} </span>
       </div>
-        <h2>{title}</h2>
-        <p><b>From ${price}</b> / person</p>
-    </div>  
+      <h2>{title}</h2>
+      <p><b>From ${price}</b> / person</p>
+    </div>
   )
 }
 
