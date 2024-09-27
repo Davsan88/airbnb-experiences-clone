bea01927afe6a1b886b29a6a8abd44b2c5abfc03
DavSan <david.sanchez.martinez@outlook.com>
2024-09-27 18:54:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,30 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
-import data from './data'
+import data from './data'  // 1. Importing the data file
 import './App.css'
 
 
 function App() {
+  // 2. Using .map() to loop over the data array and create a Card component for each item
   const cardElements = data.map(({ id, title, price, coverImg, stats, location }) => (
-    <Card key={id} title={title} price={price} coverImg={coverImg} stats={stats} location={location}/>
+    <Card 
+      key={id}
+      title={title}
+      price={price}
+      coverImg={coverImg}
+      stats={stats}
+      location={location}
+    />
   ))
 
   return (
     <>
       <Navbar />
       <Hero />
-      <Card
-        img="/katie-zaferes.png"
-        status="SOLD OUT"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zafere"
-        price={136}
-      />
+      <div>
+        { cardElements }
+      </div>
     </>
   )
 }
