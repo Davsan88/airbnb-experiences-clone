32792336da1f7ed803461f581d8f3455a57bd033
DavSan <david.sanchez.martinez@outlook.com>
2024-09-27 18:46:03 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from './data'
 import './App.css'
 
 
 function App() {
+  const cardElements = data.map(({ id, title, price, coverImg, stats, location }) => (
+    <Card key={id} title={title} price={price} coverImg={coverImg} stats={stats} location={location}/>
+  ))
+
   return (
     <>
       <Navbar />
