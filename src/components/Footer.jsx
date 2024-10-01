b2af5d5af89cb5f8a3c0b6ca806d2eabd39c6ad4
DavSan <david.sanchez.martinez@outlook.com>
2024-10-01 12:11:22 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <p>
+        airbnb experiences clone by 
+        <a href="https://davsan88.netlify.app/" target='blank' >Dav Sanmar</a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
