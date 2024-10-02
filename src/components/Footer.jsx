11e711c56912edf83ae3f1cca4a7f4ec2a3d5213
DavSan <david.sanchez.martinez@outlook.com>
2024-10-02 12:53:30 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import '../App.css'
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <img src="/airbnb-color-svgrepo-com.png" alt="airbnb icon" />  
+    <footer className="footer">
+      <img className="footer__image" src="/airbnb-color-svgrepo-com.png" alt="airbnb icon" />  
       <p>
         airbnb experiences clone by 
         <a href="https://davsan88.netlify.app/" target='blank' > Dav Sanmar</a>
